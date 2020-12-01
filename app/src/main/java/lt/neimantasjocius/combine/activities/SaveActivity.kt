384f2f66bdeda7038b916e8d05e2604d3e62a4fb
simsta6 <51540204+simsta6@users.noreply.github.com>
2020-12-01 20:21:18 +0200

    Added image saving

--- app/src/main/java/lt/neimantasjocius/combine/activities/SaveActivity.kt
+++ app/src/main/java/lt/neimantasjocius/combine/activities/SaveActivity.kt
@@ -1,23 +1,34 @@
 package lt.neimantasjocius.combine.activities
 
+import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import lt.neimantasjocius.combine.R
 import java.io.File
+import java.io.File.separator
 import java.io.FileOutputStream
+import java.io.IOException
 import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class SaveActivity : AppCompatActivity() {
+
+    var imgPath: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_save)
@@ -31,8 +42,8 @@ class SaveActivity : AppCompatActivity() {
         val filePath : String? = intent.getStringExtra("picture")
 
         save.setOnClickListener {
-            /*val bMap = BitmapFactory.decodeFile(filePath)
-            saveBitmap(bMap)*/
+            val bMap = BitmapFactory.decodeFile(filePath)
+            saveImage(bMap, this, "Combine")
         }
 
         next.setOnClickListener {
@@ -45,44 +56,60 @@ class SaveActivity : AppCompatActivity() {
         }
     }
 
-    private fun saveBitmap(bitmap : Bitmap) {
-        // Assume block needs to be inside a Try/Catch block.
+    /// @param folderName can be your app's name
+    private fun saveImage(bitmap: Bitmap, context: Context, folderName: String) {
+        if (android.os.Build.VERSION.SDK_INT >= 29) {
+            val values = contentValues()
+            values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/" + folderName)
+            values.put(MediaStore.Images.Media.IS_PENDING, true)
+            // RELATIVE_PATH and IS_PENDING are introduced in API 29.
 
-        // Assume block needs to be inside a Try/Catch block.
-        val path: String = Environment.getExternalStorageDirectory().toString()
-        var fOut: OutputStream? = null
-        val counter = 0
-        val file = File(
-            path,
-            "Combine-$counter.jpg"
-        ) // the File to save , append increasing numeric counter to prevent files from getting overwritten.
+            val uri: Uri? = context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            if (uri != null) {
+                saveImageToStream(bitmap, context.contentResolver.openOutputStream(uri))
+                values.put(MediaStore.Images.Media.IS_PENDING, false)
+                context.contentResolver.update(uri, values, null, null)
+            }
+            Toast.makeText(this, "Image saved successful.", Toast.LENGTH_SHORT).show()
+        } else {
+            val directory = File(Environment.getExternalStorageDirectory().toString() + separator + folderName)
+            // getExternalStorageDirectory is deprecated in API 29
 
-        fOut = FileOutputStream(file)
-
-        bitmap.compress(
-            Bitmap.CompressFormat.JPEG,
-            85,
-            fOut
-        )
-
-        fOut.flush() // Not really required
-
-        fOut.close() // do not forget to close the stream
-
-
-        MediaStore.Images.Media.insertImage(
-            contentResolver,
-            file.getAbsolutePath(),
-            file.getName(),
-            file.getName()
-        )
+            if (!directory.exists()) {
+                directory.mkdirs()
+            }
+            val fileName = System.currentTimeMillis().toString() + ".png"
+            val file = File(directory, fileName)
+            saveImageToStream(bitmap, FileOutputStream(file))
+            if (file.absolutePath != null) {
+                val values = contentValues()
+                values.put(MediaStore.Images.Media.DATA, file.absolutePath)
+                // .DATA is deprecated in API 29
+                context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            }
+            Toast.makeText(this, "Image saved successful.", Toast.LENGTH_SHORT).show()
+        }
     }
 
-    /*private fun saveImageToGallery() {
-        imageview.setDrawingCacheEnabled(true)
-        val b: Bitmap = imageview.getDrawingCache()
-        Images.Media.insertImage(getActivity().getContentResolver(), b, title, description)
-    }*/
+    private fun contentValues() : ContentValues {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+        values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000);
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+        return values
+    }
+
+    private fun saveImageToStream(bitmap: Bitmap, outputStream: OutputStream?) {
+        if (outputStream != null) {
+            try {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                outputStream.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+    }
 
     private fun fade(layout : ConstraintLayout) {
         val animation: Animation = AnimationUtils.loadAnimation(
