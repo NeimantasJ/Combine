659c41014cdf1707d863bd5a7eeb7f1d5d94d65f
simsta6 <51540204+simsta6@users.noreply.github.com>
2020-12-01 17:53:34 +0200

    added images history and adapter

---
+++ app/src/main/java/lt/neimantasjocius/combine/activities/ImageHistoryActivity.kt
@@ -0,0 +1,12 @@
+package lt.neimantasjocius.combine.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import lt.neimantasjocius.combine.R
+
+class ImageHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_history)
+    }
+}
